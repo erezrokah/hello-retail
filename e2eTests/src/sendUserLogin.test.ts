5da19d0ae1d1f343bef015e384f6ad3cf0b4b33f
erezrokah <erezrokah@users.noreply.github.com>
2018-10-14 11:51:06 +0300

    improve sendUserLogin test to assert lambda log

--- e2eTests/src/sendUserLogin.test.ts
+++ e2eTests/src/sendUserLogin.test.ts
@@ -1,10 +1,19 @@
+import { deleteAllLogs } from 'jest-e2e-serverless/lib/utils/cloudwatch';
 import { invoke } from 'jest-e2e-serverless/lib/utils/lambda';
 import { testsConfig } from './config';
 
 const { region } = testsConfig;
-const { eventWriter } = testsConfig.lambdas;
+const { eventWriter, processor } = testsConfig.lambdas;
 
 describe('sendUserLogin api', () => {
+  beforeEach(async () => {
+    await deleteAllLogs(region, processor);
+  });
+
+  afterEach(async () => {
+    await deleteAllLogs(region, processor);
+  });
+
   test('should log event with unsupported data schema on sendUserLogin', async () => {
     const id = 'someProfileId';
     const name = 'someProfileName';
@@ -17,6 +26,7 @@ describe('sendUserLogin api', () => {
     const body = JSON.stringify(bodyObj);
     const payload = { body };
     const result = await invoke(region, eventWriter, payload);
+
     expect(result.statusCode).toBe(200);
     expect(result.headers).toEqual({
       'Access-Control-Allow-Credentials': true,
@@ -26,5 +36,16 @@ describe('sendUserLogin api', () => {
     const actualBody = JSON.parse(result.body);
     expect(actualBody.ShardId).toStartWith('shardId');
     expect(actualBody.SequenceNumber).toMatch(/\d{10,}/);
+
+    // assert processor lambda was executed by checking its log
+    expect.assertions(5);
+    await expect({
+      function: processor,
+      pollEvery: 5000,
+      region,
+      timeout: 30 * 1000,
+    }).toHaveLog(
+      'event with unsupported data schema (com.nordstrom/user-info/login/1-0-0) observed.',
+    );
   });
 });
