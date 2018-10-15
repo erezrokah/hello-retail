b28c17971155db1b63a7cc1ef162b352fc51878d
erezrokah <erezrokah@users.noreply.github.com>
2018-10-15 14:05:29 +0300

    improve newProduct test

--- e2eTests/src/newProduct.test.ts
+++ e2eTests/src/newProduct.test.ts
@@ -1,29 +1,37 @@
 import { clearAllItems } from 'jest-e2e-serverless/lib/utils/dynamoDb';
 import { invoke } from 'jest-e2e-serverless/lib/utils/lambda';
+import { stopRunningExecutions } from 'jest-e2e-serverless/lib/utils/stepFunctions';
 import { testsConfig } from './config';
 
 const { region } = testsConfig;
 const { eventWriter } = testsConfig.lambdas;
 const { catalog, categories } = testsConfig.tables;
+const { stateMachineArn } = testsConfig;
 
-describe('sendUserLogin api', () => {
+describe('newProduct api', () => {
   const tables = [catalog, categories];
   beforeEach(async () => {
+    await stopRunningExecutions(region, stateMachineArn);
     await Promise.all(tables.map(table => clearAllItems(region, table)));
   });
 
   afterEach(async () => {
-    // await Promise.all(tables.map(table => clearAllItems(region, table)));
+    await stopRunningExecutions(region, stateMachineArn);
+    await Promise.all(tables.map(table => clearAllItems(region, table)));
   });
 
   test('should create product and category on product create', async () => {
+    // this is how the Web app generates the id
+    const id = `0000000${Math.floor(
+      Math.abs(Math.random() * 10000000),
+    )}`.substr(-7);
     const product = {
       brand: 'POLO RALPH LAUREN',
       category: 'Socks for Men',
       description: 'Best socks ever',
-      id: '4579874',
+      id,
       name: 'Polo Ralph Lauren 3-Pack Socks',
-      origin: 'hello-retail/e2e-tests-create-product',
+      origin: 'hello-retail/e2e-tests-create-product/System Test',
       schema: 'com.nordstrom/product/create/1-0-0',
     };
     const body = JSON.stringify(product);
@@ -41,7 +49,7 @@ describe('sendUserLogin api', () => {
     expect(actualBody.SequenceNumber).toMatch(/\d{10,}/);
 
     // assert processor lambda was executed by checking its log
-    expect.assertions(6);
+    expect.assertions(7);
     await expect({
       pollEvery: 5000,
       region,
@@ -64,15 +72,21 @@ describe('sendUserLogin api', () => {
     }).toHaveItem(
       { id: product.id },
       {
-        brand: 'POLO RALPH LAUREN',
-        category: 'Socks for Men',
+        brand: product.brand,
+        category: product.category,
         createdBy: product.origin,
-        description: 'Best socks evenr',
-        id: '4579874',
-        name: 'Polo Ralph Lauren 3-Pack Socks',
+        description: product.description,
+        id: product.id,
+        name: product.name,
         updatedBy: product.origin,
       },
       false,
     );
+    await expect({
+      pollEvery: 5000,
+      region,
+      stateMachineArn,
+      timeout: 30 * 1000,
+    }).toBeAtState('WaitAssignment');
   });
 });
